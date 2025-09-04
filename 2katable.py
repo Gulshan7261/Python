f72f3cf8391693ce8f7d99438678bbac9b06a5a0
Gulshan7261 <gk5120909@gmail.com>
2025-09-04 10:13:10 +0530

    py

---
+++ 2katable.py
@@ -0,0 +1,9 @@
+num = int(input("Enter the number"))
+for i in range(1,num+1):
+    print(f"{num} * {i} = {i*num}")
+
+
+bum = int(input("Enter th number"))
+n = int(bum)
+for i in range(1,11):
+    print(f"{n} * {i} = {n*i}")
---
+++ calss.py
@@ -0,0 +1,17 @@
+a = "Hello"
+b = "world"
+
+sum = a + b
+print("sentance=",sum)
+
+
+
+a = "Gulshan"
+
+
+print(a[2])
+print(a.strip())
+print(a.replace("Gulshan", "a"))
+print(a.upper())
+print(a[:4])
+print(a[-1])
